510da14c99fa4a13904648124522e521988e938e
simonrichards <simon.richards@wirvonhier.de>
2019-05-11 21:44:26 +0200

    remove console log

--- src/store/trading_pairs/reducer.js
+++ src/store/trading_pairs/reducer.js
@@ -2,7 +2,6 @@ import { initialState } from './selectors'
 import { GET_TRADING_PAIRS_SUCCESS } from "./constants"
 
 export default (state = initialState, action) => {
-    console.log('DATA REACHED REDUCER: ', action.type)
     switch (action.type) {
         case GET_TRADING_PAIRS_SUCCESS:
             return { tradingPairList: action.data }
